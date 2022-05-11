508973dd4b9c7524458b211b4cd7c4d2eede4d31
Aislan_Santos <aislan_85@yahoo.com.br>
2022-05-11 17:06:38 -0300

    implementada a rotina de valida cpf para efetuar o cadastro - só cadastra com cpf correto e por não ser um campo obrigatório ele tabém cadastra em branco

--- assets/class/contacts.class.php
+++ assets/class/contacts.class.php
@@ -99,47 +99,80 @@ class RegisterContacts
 
         public function save()
         {
-                if (!empty($this->id)) {
-                        $sql = "UPDATE contato SET name = :name, secondname = :secondname , cpf = :cpf, email = :email WHERE id = :id";
-                        $stmt = $this->pdo->prepare($sql);
-                        $stmt->bindParam(":name", $this->name, PDO::PARAM_STR);
-                        $stmt->bindParam(":secondname", $this->secondname, PDO::PARAM_STR);
-                        $stmt->bindParam(":cpf", $this->cpf, PDO::PARAM_STR);
-                        $stmt->bindParam(":email", $this->email, PDO::PARAM_STR);
-                        $stmt->bindParam(":id", $this->id, PDO::PARAM_INT);
-                        $stmt->execute();
+                if ($this->validates_CPF($this->cpf) == True) {
+                        if (!empty($this->id)) {
+                                $sql = "UPDATE contato SET name = :name, secondname = :secondname , cpf = :cpf, email = :email WHERE id = :id";
+                                $stmt = $this->pdo->prepare($sql);
+                                $stmt->bindParam(":name", $this->name, PDO::PARAM_STR);
+                                $stmt->bindParam(":secondname", $this->secondname, PDO::PARAM_STR);
+                                $stmt->bindParam(":cpf", $this->cpf, PDO::PARAM_STR);
+                                $stmt->bindParam(":email", $this->email, PDO::PARAM_STR);
+                                $stmt->bindParam(":id", $this->id, PDO::PARAM_INT);
+                                $stmt->execute();
+                        }
+                        if ($this->checkContact() == True) {
+                                $sql = "INSERT INTO contato (name, secondname, cpf, email) VALUES (:name, :secondname, :cpf, :email)";
+                                $stmt = $this->pdo->prepare($sql);
+                                $stmt->bindParam(":name", $this->name, PDO::PARAM_STR);
+                                $stmt->bindParam(":secondname", $this->secondname, PDO::PARAM_STR);
+                                $stmt->bindParam(":cpf", $this->cpf, PDO::PARAM_STR);
+                                $stmt->bindParam(":email", $this->email, PDO::PARAM_STR);
+                                $stmt->execute();
+                        }
                 }
-                if ($this->checkContact() == True){
-                        $sql="INSERT INTO contato (name, secondname, cpf, email) VALUES (:name, :secondname, :cpf, :email)";
-                        $stmt = $this->pdo->prepare($sql);
-                        $stmt->bindParam(":name", $this->name, PDO::PARAM_STR);
-                        $stmt->bindParam(":secondname", $this->secondname, PDO::PARAM_STR);
-                        $stmt->bindParam(":cpf", $this->cpf, PDO::PARAM_STR);
-                        $stmt->bindParam(":email", $this->email, PDO::PARAM_STR);
-                        $stmt->execute();
-                }
-
         }
 
         public function checkContact()
         {
-            $sql = "SELECT * FROM contato WHERE name = :name";
-            $stmt = $this->pdo->prepare($sql);
-            $stmt->bindParam(":name", $this->name, PDO::PARAM_STR);
-            $stmt->execute();
-            if ($stmt->rowCount() == 0) {
-                return true;
-            } else {
-                return false;
-            }
+                $sql = "SELECT * FROM contato WHERE name = :name";
+                $stmt = $this->pdo->prepare($sql);
+                $stmt->bindParam(":name", $this->name, PDO::PARAM_STR);
+                $stmt->execute();
+                if ($stmt->rowCount() == 0) {
+                        return true;
+                } else {
+                        return false;
+                }
         }
-        
+
         public function deleteContact()
         {
-            $sql = "DELETE FROM contato WHERE id = :id";
-            $stmt = $this->pdo->prepare($sql);
-            $stmt->bindParam(":id", $this->id, PDO::PARAM_INT);
-            $stmt->execute();
-            return true;
+                $sql = "DELETE FROM contato WHERE id = :id";
+                $stmt = $this->pdo->prepare($sql);
+                $stmt->bindParam(":id", $this->id, PDO::PARAM_INT);
+                $stmt->execute();
+                return true;
+        }
+
+        public function validates_CPF($cpf_to_be_valid = null)
+        {
+                if ($cpf_to_be_valid != null) {
+                        // Extrai somente os números
+                        $cpf_to_be_valid = preg_replace('/[^0-9]/is', '', $cpf_to_be_valid);
+
+                        // Verifica se foi informado todos os digitos corretamente
+                        if (strlen($cpf_to_be_valid) != 11) {
+                                return false;
+                        }
+
+                        // Verifica se foi informada uma sequência de digitos repetidos. Ex: 111.111.111-11
+                        if (preg_match('/(\d)\1{10}/', $cpf_to_be_valid)) {
+                                return false;
+                        }
+
+                        // Faz o calculo para validar o CPF
+                        for ($t = 9; $t < 11; $t++) {
+                                for ($d = 0, $c = 0; $c < $t; $c++) {
+                                        $d += $cpf_to_be_valid[$c] * (($t + 1) - $c);
+                                }
+                                $d = ((10 * $d) % 11) % 10;
+                                if ($cpf_to_be_valid[$c] != $d) {
+                                        return false;
+                                }
+                        }
+                        return true;
+                }else{
+                        return True;
+                }
         }
 }
