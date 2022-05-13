6885b6fce127d3f599277ab43333c3899deb0e49
Aislan_Santos <aislan_85@yahoo.com.br>
2022-05-13 11:22:44 -0300

    implementação de modal de add numeros de telefone

--- details_contact_html.php
+++ details_contact_html.php
@@ -1,4 +1,4 @@
-<?php require ("details_contact.php");?>
+<?php require("details_contact.php"); ?>
 <!DOCTYPE html>
 <html lang="pt-br">
 
@@ -7,20 +7,15 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <!-- Conteudo Bootstrap -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous">
     </script>
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js"
-        integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous">
     </script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js"
-        integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous">
     </script>
 
-    <script src="https://code.jquery.com/jquery-3.6.0.js"
-        integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.15/jquery.mask.min.js"></script>
 
 
@@ -66,8 +61,7 @@
                                                                             echo $list['cpf'];
                                                                         }
 
-                                                                        ?>" placeholder="CPF"
-                            onkeypress="$(this).mask('000.000.000-00');">
+                                                                        ?>" placeholder="CPF" onkeypress="$(this).mask('000.000.000-00');">
                     </div>
                     <div class="col mb-3">
                         <input type="email" name="email" id="email" value="<?php if (!empty($list['email'])) {
@@ -76,10 +70,8 @@
                                                                             ?>" placeholder="Email">
                     </div>
                     <div class="col mb-3">
-                        <img src="assets/img/telefone.png" alt="telefones" width="30" height="30" data-bs-toggle="modal"
-                            data-bs-target="#phone" />
-                        <img src="assets/img/receptor-de-telefone.png" alt="telefones" width="30" height="30"
-                            data-bs-toggle="modal" data-bs-target="#phone" />
+                        <img src="assets/img/telefone.png" alt="telefones" width="30" height="30" data-bs-toggle="modal" data-bs-target="#phone" />
+                        <img src="assets/img/receptor-de-telefone.png" alt="telefones" width="30" height="30" data-bs-toggle="modal" data-bs-target="#add_phone" />
                     </div>
                 </div>
             </div>
@@ -113,26 +105,49 @@
                                 $list_phone = $registerPhone->readRegister($id);
                                 foreach ($list_phone as $item) :
                             ?>
-                            <tr>
-                                <td><?php echo $item['number_phone']; ?></td>
-                                <td>Excluir</td>
-                            </tr>
-                            <?php endforeach;
+                                    <tr>
+                                        <td><?php echo $item['number_phone']; ?></td>
+                                        <td>Excluir</td>
+                                    </tr>
+                                <?php endforeach;
                             } else { ?>
-                            <tr>
-                                <td></td>
-                                <td></td>
-                            </tr>
+                                <tr>
+                                    <td></td>
+                                    <td></td>
+                                </tr>
                             <?php } ?>
                         </tbody>
                     </table>
                 </div>
+            </div>
+        </div>
+    </div>
+    
+    <!-- The Modal add phones -->
+    <div class="modal" id="add_phone">
+        <div class="modal-dialog">
+            <div class="modal-content">
 
-                <!-- Modal footer -->
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Close</button>
+                <!-- Modal Header -->
+                <div class="modal-header">
+                    <h4 class="modal-title">Adicionar Telefone: </h4>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal"></button>
                 </div>
 
+                <!-- Modal body -->
+                <div class="modal-body">
+                    <div>
+                        <form class="row g-3">
+                            <div class="col-auto">
+                                <label for="add_phone" class="visually-hidden">Telefone</label>
+                                <input type="text" class="form-control" id="add_phone" placeholder="Telefone">
+                            </div>
+                            <div class="col-auto">
+                                <button type="submit" class="btn btn-primary mb-3">Salvar</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
